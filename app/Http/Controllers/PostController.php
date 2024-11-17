7addf43ae54f2e1f00a45f5cddc3ca82c1df1705
jongkodingabi <abisamhazim04@gmail.com>
2024-11-17 17:45:29 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostDetailResource;
+use Illuminate\Http\Request;
+use App\Models\Post;
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::all();
+
+        // return response()->json(['data' => $posts]);
+        return PostResource::collection($posts);
+    }
+
+    public function show($id) 
+    {
+        $post = Post::with('writer:id,username')->findOrFail($id);
+        return new PostDetailResource($post);
+    }
+
+    public function show2($id) 
+    {
+        $post = Post::findOrFail($id);
+        return new PostDetailResource($post);
+    }
+
+    
+}
